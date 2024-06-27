eb22d6797f8fe5cf997e7c09309dd8f780f2c29d
Winogrodzki C A <x2w@milan0.ftpn.ornl.gov>
2024-06-27 13:13:11 -0400

    modified script to print bandwitdth numbers only, on separate lines,into output file. Also updates laplacian.cu to account for another arg

--- laplacian.sh
+++ laplacian.sh
@@ -5,17 +5,17 @@ L=$2 # problem size
 Bx=$3 # block dims
 By=$4
 Bz=$5
-ITER=1000
-# TEST="b"
-OUTPUT_FILE=$6
+ITER=$7 # iterations
+TEST=$6 # a = print only bandwidth, b = print full message
+OUTPUT_FILE=$8 # name of output file
 #EXEC=($PROGRAM $L $L $L $Bx $By $Bz)
 
 > $OUTPUT_FILE
 
 # Function to print a progress bar
-function ProgressBar {
+ProgressBar() {
     # Process data
-    let _progress=($1*100/$2*100)/100
+    let _progress=($1*100/1000)
     let _done=(${_progress}*4)/10
     let _left=40-$_done
 
@@ -28,11 +28,22 @@ function ProgressBar {
 
 }
 
+# Function to print a progress bar
+print_progress_bar() {
+    local percent=$(( ($1 * 100) / $2 ))
+    local done_chars=$(( ($percent * 3) / 10 ))
+    local left_chars=$(( 30 - $done_chars ))
+
+    printf "\rProgress: [%-30s] %d%%" \
+           $(printf '#%.0s' $(seq $done_chars)) \
+           $percent
+}
+
 echo "Processing for L = $L, block sizes $Bx, $By, $Bz"
 results=()
-for run in {1..$ITER}; do
-        #result=$EXEC
-        result=$($PROGRAM $N $N $N $Bx $By $Bz $TEST)
+for (( i=1; i <= $ITER; ++i ))
+do
+	result=$($PROGRAM $L $L $L $Bx $By $Bz $TEST)
 
         if [ $? -eq 0 ]; then
                 results+=($result)
@@ -41,17 +52,22 @@ for run in {1..$ITER}; do
                 #results+=("N/A")
         fi
 
-        if (( run % 10 == 0 )); then
-                ProgressBar $run $ITER
-        fi
+        #if (( $i % 10 == 0 )); then
+        #       ProgressBar $i
+	       print_progress_bar $i $ITER
+        #fi
 done
 
 echo
 
-#results_str=$(IFS=,; echo "${results[*]}")
-#echo "$N,$results_str" >> $OUTPUT_FILE
+printf "%s\n" "${results[@]}" > $OUTPUT_FILE
 
-awk -v var="${results[*]}" 'print $9' > $OUTPUT_FILE
+#awk -v var="${results[*]}" 'BEGIN { print $9 }' > $OUTPUT_FILE
+#echo ${results[*]}
 
+#for r in "${results[*]}" ; do
+	#echo "r = " $r
+#	awk -v var="$r" '{ split(var,arrayval," "); print(arrayval[9])}' > $OUTPUT_FILE
+#done
 
 echo "All results have been recorded in $OUTPUT_FILE"
